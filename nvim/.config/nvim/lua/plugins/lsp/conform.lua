59e659ac887cdcd7673bf103cf78e3f182705928
pesap <pesap@users.noreply.github.com>
2025-12-30 12:08:26 -0700

    feat: adding prettier to neovim.

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -1,6 +1,17 @@
+local prettier_config = vim.fn.stdpath("config") .. "/.prettierrc.yaml"
+
 return {
 	"stevearc/conform.nvim",
 	enabled = true,
 	tag = "stable",
-	opts = {},
+	opts = {
+		formatters_by_ft = {
+			markdown = { "prettier" },
+		},
+		formatters = {
+			prettier = {
+				prepend_args = { "--config", prettier_config },
+			},
+		},
+	},
 }
