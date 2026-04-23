cf860e4853f5a092c4fb9ae16d4a4c782640f330
pesap <pesap@users.noreply.github.com>
2026-04-22 19:20:26 -0600

    fix: tree sitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 -- Parser for file types
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
@@ -31,6 +32,50 @@ return {
 				"json",
 				"jq",
 				"kdl",
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ao"] = "@comment.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
+				},
 			},
 			incremental_selection = {
 				enable = true,
