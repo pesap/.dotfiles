0730e24173d1b9473c3974faebab7f892c5eb819
pesap <pesap@users.noreply.github.com>
2026-04-27 20:43:50 -0600

    chore(dotfiles): update nvim/alacritty and prune old zellij layouts

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,30 @@
 -- Current colorscheme of choice
 return {
-	"jpwol/thorn.nvim",
+	"metalelf0/kintsugi-nvim",
 	lazy = false,
 	priority = 1000,
-	opts = {},
 	config = function()
-		vim.cmd.colorscheme("thorn")
+		require("kintsugi").setup({
+			variant = "dark",
+			transparent = true,
+			terminal_colors = true,
+			bold_keywords = true,
+			italic_comments = false,
+		})
+		vim.cmd.colorscheme("kintsugi-dark")
+
+		vim.keymap.set("n", "<leader>tc", function()
+			local ok, fzf = pcall(require, "fzf-lua")
+			if ok then
+				fzf.colorschemes({ prompt = "Theme❯ " })
+				return
+			end
+
+			vim.ui.select({ "kintsugi-dark", "kintsugi-flared" }, { prompt = "Theme" }, function(choice)
+				if choice then
+					vim.cmd.colorscheme(choice)
+				end
+			end)
+		end, { desc = "[T]heme [C]hoose" })
 	end,
 }
