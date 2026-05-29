8bc0c0bf5c1f52e7b94bb1fbf0503c6a696eb502
pesap <pesap@users.noreply.github.com>
2026-05-29 10:21:58 -0600

    chore: bumping stuff around

--- zshrc/.config/zshrc/20-omz.zsh
+++ zshrc/.config/zshrc/20-omz.zsh
@@ -1,14 +1,34 @@
 # Oh-My-Zsh configuration
+#
+# Speed-ups via https://scottspence.com/posts/speeding-up-my-zsh-shell
+# (+ https://gist.github.com/ctechols/ca1035271ad134841284 for compinit cache)
+
+# Tell OMZ to skip the slow stuff
+DISABLE_AUTO_UPDATE="true"
+DISABLE_MAGIC_FUNCTIONS="true"
+DISABLE_COMPFIX="true"
+
+# Daily-cached compinit: rebuild the dump at most once per day, otherwise -C.
+# Runs BEFORE sourcing OMZ; OMZ will still call compinit, but the dump is fresh.
+autoload -Uz compinit
+if [[ "$(date +'%j')" != "$(stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)" ]]; then
+    compinit
+else
+    compinit -C
+fi
 
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME=""
 
 plugins=(
-    # git
     gh
     fzf
     zsh-autosuggestions
-    zsh-syntax-highlighting
+    zsh-syntax-highlighting   # must be last
 )
 
 [[ -r "$ZSH/oh-my-zsh.sh" ]] && source "$ZSH/oh-my-zsh.sh"
+
+# zsh-autosuggestions perf tweaks
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE="20"
+ZSH_AUTOSUGGEST_USE_ASYNC=1
